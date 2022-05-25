aaa98de2000b0676b2bdf023fb9327f286dfb248
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2022-05-25 18:41:01 +0300

    Basic temperature skeleton

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,10 +1,12 @@
 import { Container } from "react-bootstrap";
 import TodoItems from "../TodoItems/TodoItems";
+import TemperatureViewer from "../TemperatureViewer/TemperatureViewer";
 
 const MainContent = () => {
     return (
-        <Container className="border border-secondary rounded p-3">
+        <Container className="d-flex border border-secondary rounded p-3">
             <TodoItems />
+            <TemperatureViewer />
         </Container>
     )
 }
---
+++ src/components/TemperatureViewer/TemperatureViewer.jsx
@@ -0,0 +1,8 @@
+
+const TemperatureViewer = () => {
+    return (
+        <h2 className="m-auto text-center">Temp</h2>
+    )
+}
+
+export default TemperatureViewer;
