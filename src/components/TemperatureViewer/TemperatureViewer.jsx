95163a6c3ecd46a5427dadc8184ce5df20dbc83c
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2022-05-26 11:01:40 +0300

    Added temperature functionality

--- src/components/TemperatureViewer/TemperatureViewer.jsx
+++ src/components/TemperatureViewer/TemperatureViewer.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from 'react';
 
 const TemperatureViewer = () => {
+    const [temp, setTemp] = useState('');
+
+    const city = "Shumen";
+    const apiKey = "2d94f7e372d0465eb7272829222605";
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setTemp(data.current.temp_c));
+    }, [])
+
     return (
-        <h2 className="m-auto text-left">Temp</h2>
+        <>
+            <h2>The temperature in {city} is:</h2>
+            <h3 className="m-auto text-left">{temp} &#176;</h3>
+        </>
     )
 }
 
